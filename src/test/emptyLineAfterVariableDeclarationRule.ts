e091bc248cfa176d80814a5d9d7203b4edca5e22
Dmytro Okhrimovych <dmytro.okhrimovych@playtech.com>
2016-11-02 18:26:40 +0200

    PNG-21396 WPL3: Implement tslint rule to force new line after series of VariableDeclarations

--- src/test/emptyLineAfterVariableDeclarationRule.ts
+++ src/test/emptyLineAfterVariableDeclarationRule.ts
@@ -30,4 +30,45 @@ describe('emptyLineAfterVariableDeclaration', () => {
 
 		expect(result.failureCount).to.be.equal(1);
 	});
+
+	it('should throw error when there is no empty line after last variable statement outside class method', () => {
+		let linter = new Linter('empty-line-after-variable-declaration.ts',
+			`var outerVar1 = '1';
+var outerVar2 = '2';
+class Foo {
+    foo(var1) {
+        if (var1) {
+        	let var2 = 2;
+        	let var3;
+
+        	var3 = var2++;
+        }
+    }
+}`,
+			options);
+		let result = linter.lint();
+
+		expect(result.failureCount).to.be.equal(1);
+	});
+
+	it('should not throw error when there is empty line after each last variable statement', () => {
+		let linter = new Linter('empty-line-after-variable-declaration.ts',
+			`var outerVar1 = '1';
+var outerVar2 = '2';
+
+class Foo {
+    foo(var1) {
+        if (var1) {
+        	let var2 = 2;
+        	let var3;
+
+        	var3 = var2++;
+        }
+    }
+}`,
+			options);
+		let result = linter.lint();
+
+		expect(result.failureCount).to.be.equal(0);
+	});
 });
